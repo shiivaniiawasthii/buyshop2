4338043fb2353d1a9bcc512e3ac2d33d7598a3e3
shivani awasthi <shvaniawsthi001@gmail.com.com>
2022-12-08 12:47:59 +0530

    Routes

--- bs/src/App.js
+++ bs/src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/home/Home"
+import Contact from "./pages/contact/Contact"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+      </>
+    
   );
 }
 
